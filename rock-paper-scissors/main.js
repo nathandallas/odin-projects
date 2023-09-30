9ed69fb9e5762ddd534793100c6b5a479c7ad40f
Nathan <87546582+nathandallas@users.noreply.github.com>
2023-09-30 06:56:15 -0400

    added event listeners

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,4 +1,38 @@
 import "./style.scss";
-import { setupCounter } from "./counter.js";
 
-setupCounter(document.querySelector("#counter"));
+// Variables
+const selection = document.querySelectorAll(".choice");
+const replay = document.querySelector("#replay");
+const roundResults = document.querySelector(".round-results");
+const playerScore = document.querySelector("#player-score");
+const playerPick = document.querySelector(".player--choice");
+const computerScore = document.querySelector("#computer-score");
+const computerPick = document.querySelector(".computer--choice");
+
+let computerChoices = ["Rock", "Paper", "Scissors"];
+let playerPoints = 0;
+let computerPoints = 0;
+let playerChoice;
+
+// Functions
+
+function computerPlay() {
+  let result =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  return result;
+}
+
+
+function handleReplay() {
+  location.reload();
+}
+
+function getPlayerChoice(e) {
+  let playerSelection = e.target.id;
+  playerChoice = e.target.textContent;
+}
+
+// Event Listeners
+
+replay.addEventListener("click", handleReplay);
+selection.forEach(button => button.addEventListener("click", getPlayerChoice));
