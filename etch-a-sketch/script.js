8486f3a3cd6ea806b19425501b568a0a808661b5
Nathan <87546582+nathandallas@users.noreply.github.com>
2023-10-03 08:40:08 -0400

    completed

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,8 +1,19 @@
 const container = document.querySelector(".grid-container");
 const slider = document.getElementById("size");
+const square = document.querySelectorAll(".square");
+const clear = document.getElementById("clear");
+const blackBtn = document.getElementById("black");
+const rainbowBtn = document.getElementById("rainbow");
+const eraseBtn = document.getElementById("erase");
+
 let sizeValue = document.getElementById("size-value");
 let gridSize = slider.value;
-let color = 'black';
+
+let color = "black";
+blackBtn.onclick = () => (color = "black");
+rainbowBtn.onclick = () => (color = "rainbow");
+eraseBtn.onclick = () => (color = "erase");
+
 
 function createGrid() {
   for (j = 0; j < gridSize; j++) {
@@ -12,12 +23,12 @@ function createGrid() {
     for (i = 0; i < gridSize; i++) {
       const square = document.createElement("div");
       square.classList.add("square");
+      square.addEventListener("mouseover", changeColor);
+      square.addEventListener("mousedown", changeColor);
       row.appendChild(square);
     }
   }
 }
-
-createGrid();
 
 function handleSizeChange() {
   container.innerHTML = "";
@@ -26,4 +37,30 @@ function handleSizeChange() {
   createGrid();
 }
 
+function changeColor(e) {
+  if (color === "rainbow") {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  } else if (color === "black") {
+    e.target.style.backgroundColor = "#000000";
+  } else if (color === "eraser") {
+    e.target.style.backgroundColor = "#ffffff";
+  }
+  console.log(color);
+}
 
+function clearGrid() {
+  container.innerHTML = "";
+  createGrid();
+}
+
+clear.addEventListener("click", clearGrid);
+blackBtn.addEventListener("click", changeColor);
+rainbowBtn.addEventListener("click", changeColor);
+eraseBtn.addEventListener("click", changeColor);
+
+window.onload = () => {
+  createGrid();
+};
