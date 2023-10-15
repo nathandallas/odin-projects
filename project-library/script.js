c1f9d217d2d4fb51b3dc9d95ffbddad27f4b62ac
Nathan <87546582+nathandallas@users.noreply.github.com>
2023-10-14 22:32:05 -0400

    added toggle open and close form

--- project-library/script.js
+++ project-library/script.js
@@ -1,3 +1,15 @@
+let addBookBtn = document.querySelectorAll("#add");
+const closeFormBtn = document.querySelector("#exit");
+const addForm = document.querySelector(".form-container");
+
+function openForm() {
+  addForm.classList.add("open");
+}
+
+function closeForm() {
+  addForm.classList.remove("open");
+}
+
 const addBooks = document.querySelector(".add-books");
 const books = JSON.parse(localStorage.getItem("books")) || [];
 const booksList = document.querySelector(".book-container");
@@ -7,18 +19,20 @@ function addBook(e) {
   const title = this.querySelector("[name=title]").value;
   const author = this.querySelector("[name=author]").value;
   const pages = this.querySelector("[name=pages]").value;
+  const isRead = document.getElementById("isRead").checked;
 
   const book = {
     title,
     author,
     pages,
-    done: false,
+    isRead: false,
   };
 
   books.push(book);
   populateBooks(books, booksList);
   localStorage.setItem("books", JSON.stringify(books));
   this.reset();
+  addForm.classList.remove("open");
 }
 
 function populateBooks(items = [], itemsList) {
@@ -30,7 +44,9 @@ function populateBooks(items = [], itemsList) {
           <h4>${item.author}</h4></h4>
           <div class="book-details">
             <p>${item.pages} Pages</p>
-            <input type="checkbox" data-index=${i} id="item${i}" />
+            <input type="checkbox" data-index=${i} id="${i}" ${
+        item.isRead ? `checked` : ``
+      } />
             <label for="read">Read</label>
           </div>
         </div>
@@ -40,5 +56,13 @@ function populateBooks(items = [], itemsList) {
 }
 
 addBooks.addEventListener("submit", addBook);
+
+
+for (var i = 0; i < addBookBtn.length; i++) {
+  addBookBtn[i].addEventListener("click", openForm);
+}
+
+
+closeFormBtn.addEventListener("click", closeForm);
 
 populateBooks(books, booksList);
